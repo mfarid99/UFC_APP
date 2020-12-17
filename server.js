56da9c8c2e71b79764cc61181cd99ddcf50d90c0
moe <moe.faridd@gmail.com>
2020-12-16 22:39:12 -0500

    commit 2: models done

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 ///////////////////////////
 // Environmental Variables
 ///////////////////////////
+// REMEMBER TO CREATE .env file
 require("dotenv").config();
 const { PORT, SECRET } = process.env;
 
-//Dependencies
+///////////////////////////
+// Dependencies
+//////////////////////////
 
-//Bringing In Express
+// Bringing in Express
 const express = require("express");
 const app = express();
 
@@ -14,37 +17,41 @@ const app = express();
 const mongoose = require("./db/dbconn");
 
 // ROUTERS
-const authRouter = require("./controllers/auth");
-const testRouter = require("./controllers/test");
-const noteRouter = require("./controllers/notes")
+// // const authRouter = require("./controllers/auth");
+// const testRouter = require("./controllers/test");
+// const noteRouter = require("./controllers/notes")
 
 // OTHER IMPORTS
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-const methodOverride = require("method-override");
-const morgan = require("morgan");
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo")(session);
+// const methodOverride = require("method-override");
+// const morgan = require("morgan");
+
+///////////////////////////////
+// Set View Engine
+///////////////////////////////
 
 //set view engine to express-react-views
 app.set("view engine", "jsx");
-app.engine("jsx", require("express-react-views").createEngine());
+// app.engine("jsx", require("express-react-views").createEngine());
 
 ////////////
 //MIDDLEWARE
 ////////////
 // SESSIONS, this allows you to use req.session for tracking session data
-app.use(
-  session({
-    secret: SECRET,
-    saveUninitialized: false, // don't create session until something stored
-    resave: false, //don't save session if unmodified
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
-  })
-);
+// app.use(
+//   session({
+//     secret: SECRET,
+//     saveUninitialized: false, // don't create session until something stored
+//     resave: false, //don't save session if unmodified
+//     store: new MongoStore({ mongooseConnection: mongoose.connection }),
+//   })
+// );
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true })); //comment if not using forms
-app.use(methodOverride("_method"));
+// app.use(methodOverride("_method"));
 // app.use(express.json()) uncomment if using json
-app.use(morgan("tiny")); //logging
+// app.use(morgan("tiny")); //logging
 
 ///////////////
 //Routes and Routers
@@ -53,9 +60,9 @@ app.get("/", (req, res) => {
   res.render("notes/show.jsx", { hello: "Hello World" });
 });
 
-app.use("/auth", authRouter);
-app.use("/test", testRouter);
-app.use("/notes", noteRouter)
+// // app.use("/auth", authRouter);
+// app.use("/test", testRouter);
+// app.use("/notes", noteRouter)
 
 ////////////////////////
 //APP LISTENER
@@ -63,4 +70,3 @@ app.use("/notes", noteRouter)
 app.listen(PORT, () => {
   console.log(`Your are listening on port ${PORT}`);
 });
-
